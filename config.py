b967b470ade24eb18985c614a5f0614d0987423d
nezl0i <44155653+nezl0i@users.noreply.github.com>
2021-12-30 10:08:01 +0300

    Delete config.py

--- config.py
+++
@@ -1,8 +0,0 @@
-START_PASSWORD = 139240
-STOP_PASSWORD = 999999
-PASS_MODE = 'hex'   # 'hex' or 'ascii'
-PK = 42  # Default '0'
-SERIAL_TIMEOUT = .7
-PORT = 'COM20'   # Serial port Linux-'/dev/tty' Windows-'COM1'
-CSD = True  # CSD mode
-PHONE = '+79613358025'  # if CSD==True
